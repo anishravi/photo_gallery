5eab8635564fbe967062d0a44b6d4970ebdcdc58
anishravi <anishravi2002@gmail.com>
2024-06-17 17:08:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import FetchData from "./components/FetchData";
+
+const App = () => {
+  return(
+    <FetchData />
+  )
+};
+
+export default App;
---
+++ src/components/FetchData.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+const FetchData = () => {
+  const [state, setState] = useState([]);
+  const url = "https://picsum.photos/v2/list?page=";
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetch(url + page)
+      .then((response) => response.json())
+      .then((data) => {
+        let datawithLikes = data.map((item)=>({...item,like:0}))
+        setState((prevState) => [...prevState, ...datawithLikes]);
+      });
+  }, [page]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight
+    ) {
+      setPage((previousPage) => previousPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleLike = (id) => {
+    setState((prevState) =>
+      prevState.map((item) =>
+        item.id === id ? { ...item, like: item.like + 1 } : item
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1 style={{ marginLeft: "950px" }}>Photo Gallery</h1>
+      <div className="mainContainer">
+        {state?.map((item) => {
+          return (
+            <div className="container" key={item.id}>
+              <img src={item.download_url} height={250} width={250} alt="" />
+              <p>Author: {item.author}</p>
+              <i class="fa-solid fa-thumbs-up" onClick={()=>handleLike(item.id)} style={{ color: "#0c6df9" }} /><p>{item.like === 0 ? "" : item.like}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FetchData;
